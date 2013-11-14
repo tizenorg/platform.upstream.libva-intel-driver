6053465b6a0d9096d5659109318777b8e9da8947
Xiang, Haihao <haihao.xiang@intel.com>
2013-11-14 15:48:59 +0800

    Enlarge the size of array misc_param
    And check the type before storing misc parameters
    
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

--- src/i965_drv_video.c
+++ src/i965_drv_video.c
@@ -2152,6 +2152,9 @@ i965_encoder_render_misc_parameter_buffer(VADriverContextP ctx,
     assert(obj_buffer->buffer_store->bo == NULL);
     assert(obj_buffer->buffer_store->buffer);
 
+    if (param->type > ARRAY_ELEMS(encode->misc_param))
+        return VA_STATUS_ERROR_INVALID_PARAMETER;
+
     param = (VAEncMiscParameterBuffer *)obj_buffer->buffer_store->buffer;
     i965_release_buffer_store(&encode->misc_param[param->type]);
     i965_reference_buffer_store(&encode->misc_param[param->type], obj_buffer->buffer_store);
