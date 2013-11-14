55471d4057d89197c11a633f986a115a39cf86bb
Xiang, Haihao <haihao.xiang@intel.com>
2013-11-14 16:19:36 +0800

    Fix the error in the previous commit
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

--- src/i965_drv_video.c
+++ src/i965_drv_video.c
@@ -2152,10 +2152,11 @@ i965_encoder_render_misc_parameter_buffer(VADriverContextP ctx,
     assert(obj_buffer->buffer_store->bo == NULL);
     assert(obj_buffer->buffer_store->buffer);
 
+    param = (VAEncMiscParameterBuffer *)obj_buffer->buffer_store->buffer;
+
     if (param->type > ARRAY_ELEMS(encode->misc_param))
         return VA_STATUS_ERROR_INVALID_PARAMETER;
 
-    param = (VAEncMiscParameterBuffer *)obj_buffer->buffer_store->buffer;
     i965_release_buffer_store(&encode->misc_param[param->type]);
     i965_reference_buffer_store(&encode->misc_param[param->type], obj_buffer->buffer_store);
 
